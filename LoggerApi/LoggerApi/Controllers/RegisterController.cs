6551873c331d4fe824c40016468502a338dea383
Aneudy Guzman <aneguzman@gmail.com>
2016-07-01 18:25:26 -0400

    Initial commit

---
+++ LoggerApi/LoggerApi/Controllers/RegisterController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace LoggerApi.Controllers
+{
+    public class RegisterController : ApiController
+    {
+        public string Get()
+        {
+            return "Hola";
+        }
+
+        public string Post()
+        {
+            return null;
+        }
+    }
+}
